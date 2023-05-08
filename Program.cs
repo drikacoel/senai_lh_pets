1517dd5b6b942c537d5a9a426eea00d08766ded2
drikacoel <113476965+drikacoel@users.noreply.github.com>
2023-05-08 11:09:28 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+namespace Projeto_Web_Lh_Pets_versao_1;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        var app = builder.Build();
+
+        app.MapGet("/", () => "Projeto web LH Pets versão 1!");
+
+        app.UseStaticFiles();
+        app.MapGet("/index", (HttpContext contexto)=> {
+            contexto.Response.Redirect("index.html", false);
+
+        } );
+
+        Banco dba=new Banco();
+app.MapGet("/listaClientes", (HttpContext contexto) =>{
+contexto.Response.WriteAsync( dba.GetListaString());
+});
+
+
+        app.Run();
+    }
+}
